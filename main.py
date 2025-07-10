98ac047ed4435ee7e136dab9de8bdb91c6ff76d9
the-real-josh <142699808+the-real-josh@users.noreply.github.com>
2025-07-10 12:58:40 -0500

    velocity triangles work

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def pol_to_comps(mag, ang, unit='rad'):
     """
@@ -32,46 +34,81 @@ def comps_to_pol(vec, out_unit='rad'):
 
     return mag, ang
 
+
 class V_triangle:
-    # should be eliminated because you do the transform first or second depending on whether it is a rotor or stator.
-    # do it stage based instead idiot
-    # also look back at your 351 notes for this, idiot
-    # also look back at the class notes for this, idiot
-    # stop being so stupid.
-    def __innit__(self, c1, U, beta):
+    # process:
+    #   take the inlet velocity
+    #   adjust it to the frame of reference of the cascade (be it rotor or stator)
+    #   calculate deflection
+    #   flow leaves (all outflows are relative to the cascade)
+    def __init__(self, v_inlet, v_blade, turn_angle):
         """c is the vector velocity of the incoming flow. Type must be array of dimension 2
             u is the speed of the rotor"""
         
-        # check types
-        assert isinstance(c1, np.ndarray)
-        assert isinstance(U, float)
-        assert isinstance(beta, float)
+        # velocity of the blade (positive means right to left motion of the blade)
+        # I expect rotors to be positive and stators to be negative
+        assert isinstance(v_blade, float)
+        self.v_blade = v_blade # 52ish?
 
-        # assign input values
-        self.c1 = c1 # absolute velocity
-        self.U = U
-        self.beta = beta
+        # v_inlet = 0, 30
+        # velocity of the air at the inlet, adjusted for blade velocity
+        assert isinstance(v_inlet, np.ndarray), type(v_inlet)
+        self.rel_v_inlet = np.array([v_inlet[0] + v_blade,  
+                                     v_inlet[1]])
 
-        # calculate values in frame a
-        self.v1 = c1-U
-        self.
+        # angle of the turning angle in radians
+        assert isinstance(turn_angle, float)
+        self.turn_angle = turn_angle
 
+        # calculate relative exit velocity
+        rotation_induced_angle = np.atan2(self.rel_v_inlet[0], self.rel_v_inlet[1]) # angle of attack relative to the blade
+        outlet_angle = rotation_induced_angle + turn_angle
+        self.v_outlet = np.array([v_inlet[1]*np.tan(np.pi/2 - outlet_angle),
+                                  v_inlet[1]])
  
-    def plot(self):
+        print(f'initial angle: 0\neffective angle after transform: { 180/3.14*np.atan2(self.rel_v_inlet[0], self.rel_v_inlet[1])}\nplus turning angle: {180/3.14*outlet_angle}\noutlet velocity: {self.v_outlet}')
+
+    def plot(self, title='velocity triangle', verbose=True):
         # for debugging/viewing
-        assert False, "stub"
-        return -1
+
+        print(f'absolute inlet velocity: {'amogus'}\n\
+              relative inlet velocity: {self.rel_v_inlet}\n\
+              outlet velocity: {self.v_outlet}')
+
+        fig, ax = plt.subplots()
+
+        ax.set_xscale('linear')
+        ax.set_yscale('linear')
+
+        # arrow because quiver is comically broken
+        ax.arrow(0, 0,                     0,                      30,                     color='k', head_width=0.5, head_length=1.0)
+        ax.arrow(0, 0,                     self.rel_v_inlet[0],    self.rel_v_inlet[1],    color='r', head_width=0.5, head_length=1.0)
+        ax.arrow(0, self.rel_v_inlet[1],   self.v_blade,           0,                      color='g', head_width=0.5, head_length=1.0)
+        ax.arrow(0, 0,                     self.v_outlet[0],       self.v_outlet[1],       color='b', head_width=0.5, head_length=1.0)
+        fig.suptitle(f'{title}') # broken
+
+        if verbose:
+            plt.show()
+        elif not verbose:
+            plt.savefig(f'{title}.png')
+            plt.clf()
 
 class Stage:
-    def __innit__(self, c, what_info_do_i_need):
+    def __init__(self, c, rpm, r):
         # rotor
-        rotor = V_triangle(c=c)
-        rotor.out_velocity()
+        v_blade = (rpm*6.28/3600)*(r) # v = omega*r
+
+        rotor = V_triangle(c, v_blade, np.deg2rad(20))
+        v2 = rotor.v_outlet()
 
         # stator
+        stator = V_triangle(v2, -v_blade, -np.deg2rad(20))
+        v3 = stator.v_outlet()
+
     def get_dhn(self):
         print(f'need to get the de hallard number')
-    def get
+    def get_stuff(self):
+        print(f'cuh')
 
 class Compressor:
     # stack the stages together
@@ -79,12 +116,22 @@ class Compressor:
     #   overall pressure ratio
     #   number of stages
     #   
-    def __innit__(self):
+    def __init__(self):
         pass
 
 def main():
     # calculate dimensions
-    # 
+    # do something
+
+    # velocity triangles!
+    v_blade = (20000*6.28/3600)*(1.5) # v = omega*r
+
+    rotor = V_triangle(np.array([0.0, 30.0]), v_blade, np.deg2rad(20))
+    v2 = rotor.v_outlet
+    rotor.plot()
+
+if __name__ == "__main__":
+    main()
 pi_oc = 4.15 # compressure overall pressure ratio
 m_dot = 20 # air mass flow, kg/sec
 TIT = 1100 # turbine inlet temperature, kelvin
